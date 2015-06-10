788a397b47308e966ff90fe04e99417a8f8df673
Walter Gandarella <walter.wgbn@gmail.com>
2015-06-10 20:34:10 -0300

    hoje

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 ;(function(window, document){
 
+    'use strict';
+
     var App = (function(){
 
         var _private = {};
         var _public = {};
 
+        var itens = [
+            {
+                item: 'Biscoito de chocolate',
+                valor: 1.5,
+                qtde: 2
+            },
+            {
+                item: 'Bolacha maizena',
+                valor: 2.8,
+                qtde: 1
+            }
+        ];
+
         _public.init = function(){
             console.info('init');
+
+            console.info(itens);
+
+            var _altura = document.querySelector('body').offsetHeight - (document.querySelector('header').offsetHeight + document.querySelector('.barra').offsetHeight);
+            document.querySelector('.app').style.maxHeight = (_altura-60)+'px';
+        };
+
+        _private.printListaItens = function(){
+
         };
 
         return _public;
