f325eaf44b7b9a0474c7d0d59a05e78864c486c9
Walter Gandarella <walter.wgbn@gmail.com>
2015-06-16 23:16:05 -0300

    progresso

--- app.js
+++ app.js
@@ -20,6 +20,8 @@
 
             var _altura = document.querySelector('body').offsetHeight - (document.querySelector('header').offsetHeight + document.querySelector('.barra').offsetHeight);
             document.querySelector('.app').style.maxHeight = (_altura-60)+'px';
+            document.querySelector('.add').addEventListener('click', _private.addClick, false);
+            document.querySelector('.formulario form').addEventListener('submit', _private.addItemLista, false);
 
             _private.printListaItens();
         };
@@ -55,18 +57,59 @@
             document.querySelector('.lista').innerHTML = _html;
             document.querySelector('#total').innerHTML = _total;
         };
-        _private.addItemLista = function() {
+        
+        _private.addItemLista = function(e) {
+            e.preventDefault();
+            
             var item = {
                 item: document.querySelector("#nome-item").value,
                 valor: document.querySelector("#valor-item").value,
                 qtde: document.querySelector("#qtd-item").value
             };
-
-            itens.push(item);
-        }; 
+            
+            if (item.item && item.valor && item.qtde){
+                
+                itens.push(item);
+                
+                document.querySelector('.app').style.display = 'block';
+                document.querySelector('.formulario').style.display = 'none';
+                
+                _private.printListaItens();
+                _private.limpaFormulario();
+                
+            } else {
+                
+                _private.toast("Preencha todos os campos");
+                
+            }
+        };
+        
+        _private.limpaFormulario = function() {
+            var inputs = document.querySelectorAll('.formulario form input');
+            for (var i = 0; i < inputs.length - 1; i++)
+                inputs[i].value = '';
+        };
+        
+        _private.addClick = function(e){
+            document.querySelector('.app').style.display = 'none';
+            document.querySelector('.formulario').style.display = 'block';
+            document.querySelector('.add').innerHTML = 'ok';
+        };
+        
+        _private.toast = function(_texto){
+            var _htmlToast = document.querySelector('.toast');
+            _htmlToast.querySelector('span').innerHTML = _texto;
+            _htmlToast.style.opacity = 1;
+            
+            window.setTimeout(function(e){
+                _htmlToast.style.opacity = 0;
+            }, 3000);
+        };
+        
         return _public;
 
     })();
+
 
     window.App = App;
 
